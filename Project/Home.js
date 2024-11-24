3dd74c6d0a3430ce620cf0108b6351d12a20461e
angeltamuc <acampos21@leomail.tamuc.edu>
2024-11-23 23:33:46 -0600

    Create Home.js

---
+++ Project/Home.js
@@ -0,0 +1,13 @@
+//Goes in Pages
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>Welcome! This is the home page! ;) </p>
+        </div>
+    );
+};
+
+export default Home;
