8035fdd3f232995931aeb5b01fcc315d05f71d3f
angeltamuc <acampos21@leomail.tamuc.edu>
2024-12-02 22:50:51 -0600

    Update App.js

--- Project/App.js
+++ Project/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import DiscussionsPage from './pages/DiscussionsPage';
 import Account from './pages/Account';
 import './App.css';
 // npm install react-router-dom
@@ -13,12 +14,16 @@ const App = () => {
                         <Link to="/">Home</Link>
                     </li>
                     <li>
+                        <Link to="/discussions">Discussions</Link>
+                    </li>
+                    <li>
                         <Link to="/account">Account</Link>
                     </li>
                 </ul>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/discussions" element={<DiscussionsPage />} />
                 <Route path="/account" element={<Account />} />
             </Routes>
         </Router>
