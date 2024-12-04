0164b8a7732b35258d60a2d05a53944026f4e4a2
angeltamuc <acampos21@leomail.tamuc.edu>
2024-12-03 20:19:28 -0600

    Update Account.js

--- Project/Account.js
+++ Project/Account.js
@@ -1,16 +1,1 @@
-//goes in Pages
-import React from 'react';
-import DataFetcher from '../DataFetcher';
-
-const Account = () => {
-    return (
-        <div>
-            <h1>Login into your Account here</h1>
-            <p>Here is your info from MongoDB: </p>
-            <DataFetcher/>
-
-        </div>
-    );
-};
-
-export default Account;
+//nvm not using
