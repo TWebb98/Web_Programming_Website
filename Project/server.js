436ac9b089a5959dd14f62aa11d02b96f76984b7
angeltamuc <acampos21@leomail.tamuc.edu>
2024-12-03 21:43:49 -0600

    Update server.js

--- Project/server.js
+++ Project/server.js
@@ -22,7 +22,7 @@ mongoose.connection.on('error', (err) => {
 // Create the Hapi server
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
+    port: 3001,
     host: '0.0.0.0',
   });
 
