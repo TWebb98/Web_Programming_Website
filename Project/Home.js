32d28aa13a63ca2d0469f43c7e3770beb0f1f2b6
angeltamuc <acampos21@leomail.tamuc.edu>
2024-12-06 19:19:34 -0600

    Update Home.js

--- Project/Home.js
+++ Project/Home.js
@@ -1,11 +1,14 @@
 //Goes in Pages
 import React from 'react';
+import logo from '../assets/logo.png'; // Adjust the path as needed
 
 const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
-            <p>Welcome! This is the home page! ;) </p>
+            <p>Welcome to Vapour Discussions, a discussion board for all your favorite games! ;) </p>
+            <img src={logo} alt="Vapour Logo" style={{ width: '600px', height: 'auto' }} />
+            <p>Simply click on the Discussions page to start your discussion!</p>
         </div>
     );
 };
